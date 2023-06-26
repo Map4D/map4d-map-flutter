495f3c08e7ac554c3370c7c9578d549465b8c720
Huy Dang <caohuy@live.com>
2023-06-26 08:46:10 +0700

    Move samples to https://github.com/map4d/flutter-samples

--- samples/auto_suggest/android/app/src/main/kotlin/com/example/auto_suggest/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.auto_suggest
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- samples/custom_marker_infowindow/android/app/src/main/kotlin/com/example/custom_marker_infowindow/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.custom_marker_infowindow
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
