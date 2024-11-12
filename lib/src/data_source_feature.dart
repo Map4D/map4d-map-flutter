c0f0b798db1250bd6688c5aa67d44032246b25a8
Huy Dang <caohuy@live.com>
2024-11-12 16:19:53 +0700

    Remove unused properties

--- lib/src/data_source_feature.dart
+++ lib/src/data_source_feature.dart
@@ -1,15 +1,11 @@
 class MFDataSourceFeature {
   const MFDataSourceFeature(
-    this.id,
-    this.name,
     this.source,
     this.sourceLayer,
     this.layerType,
     this.properties,
   );
 
-  final String id;
-  final String name;
   final String source;
   final String sourceLayer;
   final String layerType;
@@ -21,8 +17,6 @@ class MFDataSourceFeature {
     }
 
     return MFDataSourceFeature(
-      json['id'],
-      json['name'],
       json['source'],
       json['sourceLayer'],
       json['layerType'],
